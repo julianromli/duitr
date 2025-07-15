f465a2b8fa987c951fa820aef6bdb0ed71eeb2cb
julianromli <faizintfd@gmail.com>
2025-07-15 10:54:39 +0700

    feat(ui): add typewriter animation and motion effects to landing page
    - Implement Typewriter component for dynamic text animation in hero section
    - Add framer-motion animations to header, footer and content sections
    - Update service worker cache revision for index.html

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-6c6c3b19'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.hi46ev6knj8"
+    "revision": "0.v6gaap51h68"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
